a508350f50beed02e8f6cce479dbc3dddc1ffa06
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-06-26 13:33:48 -0500

    onAuth added and tranasction is confirmed

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -24,10 +24,15 @@ export default {
             }
           }]
         });
+      },
+      onApprove: function(data, actions) {
+        return actions.order.capture().then(function(details) {
+          // Show a success message to the buyer
+          alert('Transaction completed by ' + details.payer.name.given_name + '!');
+        });
       }
     }).render('#paypal-button-container')
 
-    console.log(window.paypal)
   }
 }
 
