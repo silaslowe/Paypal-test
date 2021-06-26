de41918bf98ed3d7851b07d6a28ba5101fbc9811
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-06-26 11:35:22 -0500

    page word in html

---
+++ src/components/Payment.vue
@@ -0,0 +1,44 @@
+<template>
+  <div ref="root">
+    <h1>Pals</h1>
+    <div id="paypal-button-container"></div>
+  </div>
+</template>
+
+<script>
+// import { ref, onMounted } from 'vue'
+
+// import {onMounted} from "vue";
+
+
+export default {
+  setup() {
+    // paypal.Buttons({
+    //   createOrder: function(data, actions) {
+    //     // Set up the transaction
+    //     return actions.order.create({
+    //       purchase_units: [{
+    //         amount: {
+    //           value: '0.01'
+    //         }
+    //       }]
+    //     });
+    //   }
+    // }).render('#paypal-button-container')
+  }
+}
+
+    // onMounted(() => {
+    //   let recaptchaScript = document.createElement('script')
+    //   recaptchaScript.setAttribute('src', 'https://www.paypal.com/sdk/js?client-id=AYk9nOjy_151hQwR4HDqpytyOzZPOrff1SmXR4qGCC_CcHWH_ElCTOAL6-tVbl-mIM9twh-UPaKCGErU&components=buttons')
+    //   document.head.appendChild(recaptchaScript)
+    //
+    //   console.log(recaptchaScript)
+    // })
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Payment />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Payment from '@/components/Payment.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Payment
   }
 }
 </script>
